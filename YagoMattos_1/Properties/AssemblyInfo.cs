ab06b53560242dd0029d8b58430fb9e20990de71
yagopmtt <yagomattosg@gmail.com>
2023-05-30 15:55:45 -0300

    Adicionar arquivos de projeto.

---
+++ YagoMattos_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YagoMattos_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("YagoMattos_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2CE43A92-95FA-448B-AD9D-5EBA247E97BD")]
+[assembly: AssemblyVersion("1.0.0.0")]
